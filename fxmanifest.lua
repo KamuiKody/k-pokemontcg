d8233ba00b4327b61adadc2bb22a6b0a4092aaf2
KamuiKody <62488881+KamuiKody@users.noreply.github.com>
2021-07-11 21:05:17 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,19 +1,30 @@
 name "cards"
 author "KamuiKody"
 
+ui_page "html/index.html"
+
 fx_version "cerulean"
 game "gta5"
 
-shared_script '@qb-core/import.lua'
-
+shared_script "@qb-core/import.lua"
 
 client_scripts {
     '@menuv/menuv.lua',
-    'client.lua',
-    'config.lua'
+    'config.lua',
+    'client.lua'
 }
 
-server_scripts { 
-    'server.lua',
-    'config.lua'
+server_scripts {
+ 'config.lua',
+ 'server.lua',
 }
+
+files {
+ 'html/index.html',
+ 'html/img/*.png',
+ 'html/script.js',
+ 'html/style.css',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/booster_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/prop_deckbox_01.ytyp'
